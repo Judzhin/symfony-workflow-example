6b360507b41a69a7f7b44a806871779e853b0413
Judzhin Miles <info@msbios.com>
2025-08-19 14:41:17 +0300

    Add Post Controller

---
+++ src/Controller/PostController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class PostController extends AbstractController
+{
+    #[Route('/post', name: 'app_post')]
+    public function index(): Response
+    {
+        return $this->render('post/index.html.twig', [
+        ]);
+    }
+
+    public function view(): Response
+    {
+        return $this->render('post/view.html.twig', [
+        ]);
+    }
+}
